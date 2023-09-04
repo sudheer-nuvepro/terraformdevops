17bf3c0864e8b410764f9c1d1ce112dfe45092c4
sudheer-nuvepro <115560467+sudheer-nuvepro@users.noreply.github.com>
2023-09-04 16:42:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "Test"
+  location = "eastus"
+}
+resource "azurerm_virtual_network" "example" {
+  name                = "Test_vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
 
